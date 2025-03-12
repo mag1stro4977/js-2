be158946892d0c8f7c58b7f5e8ce8ff20352697b
mikey? <serbekisroilov673@gmail.com>
2025-03-12 15:52:19 +0500

    Add files via upload

---
+++ jo.js
@@ -0,0 +1,9 @@
+const sayAge = prompt('сколько фильмов вы смотрели?')
+
+if (sayAge <= 5 && sayAge > 10) {
+    console.log('вы не любите кино');
+} else if (sayAge = 10 && sayAge > 20) {
+    console.log('вы любите кино');
+} else if (sayAge > 20) {
+    console.log('вы киноман');
+}
